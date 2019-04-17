2326413ebd86d8f9d277332fc3e0f234acc618e1
GeniusJiangLing <37114625+GeniusJiangLing@users.noreply.github.com>
2019-04-18 01:00:51 +0800

    设置系统托盘和windows通知

--- renderer/index.js
+++ renderer/index.js
@@ -2,12 +2,23 @@
  author:  JiangL
  date:    2019年04月17日
  */
+
 $(function () {
-    laydate.render({
-        elem: '#test2',
-        position: 'static',
-        change: function (value, date) { //监听日期被切换
-            lay('#testView').html(value);
-        }
+    layui.use('laydate', function () {
+        const ins1 = layui.laydate.render({
+            elem: '#calendar',
+            position: 'static',
+            mark: {
+                '2019-4-18': '测试日期'
+            },
+            change: function (value, date) {
+
+            },
+            done: function (value, date) {
+                if (date.year === 2019 && date.month === 4 && date.date === 18) { //点击2017年8月15日，弹出提示语
+                    ins1.hint(value);
+                }
+            }
+        });
     });
 });
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu, Tray} = require('electron');
+const electron = require('electron');
+const fs = require('fs');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+
+let tray;
 
 function createWindow() {
     // Create the browser window.
@@ -11,7 +15,7 @@ function createWindow() {
         width: 1200,
         height: 800,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
         }
     });
 
@@ -19,34 +23,42 @@ function createWindow() {
     mainWindow.loadFile('page/index.html');
 
     // Open the DevTools.
-    // mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null
     });
+
+    // 设置系统托盘
+    tray = new Tray("static/img/lufi.ico");
+    const contextMenu = Menu.buildFromTemplate([
+        {label: '退出程序', type: 'normal', click: () => app.quit()}
+    ]);
+    tray.setToolTip('贼鸡儿好用的软件');
+    tray.setContextMenu(contextMenu);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') app.quit()
+    /*if (process.platform !== 'darwin') {
+        app.quit();
+    }*/
+    tray.displayBalloon({
+        title: '贼鸡儿好用的软件',
+        content: '程序已最小化到系统托盘！',
+        icon: 'static/img/lufi.jpg'
+    });
 });
 
 app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) createWindow()
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+electron.ipcMain.on('readJson', function (event, args) {
+    fs.readFile(args, 'utf8', function (err, data) {
+        event.sender.send('readJson', data);
+    })
+});
